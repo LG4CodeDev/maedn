69f688f961560fa8749b20bcd44dcaf52c5215e9
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2022-04-03 13:33:05 +0200

    example login (later deleted)

---
+++ Login Beispiel/login.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+var server = app.listen(3000);
+
+
+hi
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+app.use('/test', express.static(path.join(__dirname, 'static/sketch/index.html')))
+// http://localhost:3000/
+app.get('/', function(request, response) {
+    // Render login template
+    response.sendFile(path.join(__dirname + '/static/login.html'));
+});
+
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+    // If the user is loggedin
+    if (request.session.loggedin) {
+        // Output username
+        //response.send('Welcome back, ' + request.session.username + '!');
+        response.send('Hello' + request.session.username);
+    } else {
+        // Not logged in
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+    // Capture the input fields
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exists and are not empty
+    if (username && password) {
+        // Execute SQL query that'll select the account from the database based on the specified username and password
+        //connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            // If there is an issue with the query, output the error
+            //if (error) throw error;
+            // If the account exists
+            if (username == 'benny') {
+                // Authenticate the user
+                request.session.loggedin = true;
+                request.session.username = username;
+                // Redirect to home page
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        //});
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io')(server);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+    // We are given a websocket object in our function
+    function (socket) {
+
+        console.log("We have a new client: " + socket.id);
+
+        // When this user emits, client side: socket.emit('otherevent',some data);
+        socket.on('mouse',
+            function(data) {
+                // Data comes in as whatever was sent, including objects
+                console.log("Received: 'mouse' " + data.x + " " + data.y);
+
+                // Send it to all other clients
+                socket.broadcast.emit('mouse', data);
+
+                // This is a way to send to everyone including sender
+                // io.sockets.emit('message', "this goes to everyone");
+
+            }
+        );
+
+        socket.on('disconnect', function() {
+            console.log("Client has disconnected");
+        });
+    }
+);
+
+
