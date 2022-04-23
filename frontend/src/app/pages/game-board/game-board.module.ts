d6a29218f07ce1c3828c3d8308ba7b990db9a103
aschulte201 <a.schulte201@gmail.com>
2022-04-23 12:20:43 +0200

    basic grid and non working dice

--- frontend/src/app/pages/game-board/game-board.module.ts
+++ frontend/src/app/pages/game-board/game-board.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GameBoardRoutingModule } from './game-board-routing.module';
+import {GameBoardComponent} from "./game-board/game-board.component";
+import {NzGridModule} from "ng-zorro-antd/grid";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GameBoardComponent
+  ],
   imports: [
     CommonModule,
-    GameBoardRoutingModule
+    GameBoardRoutingModule,
+    NzGridModule
   ]
 })
 export class GameBoardModule { }
--- frontend/src/app/pages/game-board/game-board/game-board.component.ts
+++ frontend/src/app/pages/game-board/game-board/game-board.component.ts
@@ -2,7 +2,96 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-board',
-  templateUrl: './game-board.component.html',
+  //templateUrl: './game-board.component.html',
+  template: `
+    <div nz-row>
+      <div nz-col class="side" nzFlex="auto">
+
+      </div>
+      <div nz-col class="game" nzFlex="15">
+          <div nz-row class="row">
+            <div nz-col class="board" id="ul"><h1>1.1</h1></div>
+            <div nz-col class="board" id="um"><h1>1.2</h1></div>
+            <div nz-col class="board" id="ur"><h1>1.3</h1></div>
+          </div>
+          <div nz-row class="row">
+            <div nz-col class="board" id="ml"><h1>2.1</h1></div>
+            <div nz-col class="board" id="mm"><h1>2.2</h1></div>
+            <div nz-col class="board" id="mr"><h1>2.3</h1></div>
+          </div>
+          <div nz-row class="row">
+            <div nz-col class="board" id="bl"><h1>3.1</h1></div>
+            <div nz-col class="board" id="bm"><h1>3.2</h1></div>
+            <div nz-col class="board" id="br"><h1>3.3</h1></div>
+          </div>
+      </div>
+      <div nz-col class="side" nzFlex="auto">
+        <div nz-row></div>
+        <div nz-row></div>
+        <div nz-row id="dice">
+          <div class = 'dice-container'>
+            <div class='dice'>
+              <div class='face' data-id='1'>
+                <div class="point point-middle point-center">
+                </div>
+              </div>
+              <div class='face' data-id='2'>
+                <div class="point point-top point-right">
+                </div>
+                <div class="point point-bottom point-left">
+                </div>
+              </div>
+              <div class='face' data-id='6'>
+                <div class="point point-top point-right">
+                </div>
+                <div class="point point-top point-left">
+                </div>
+                <div class="point point-middle point-right">
+                </div>
+                <div class="point point-middle point-left">
+                </div>
+                <div class="point point-bottom point-right">
+                </div>
+                <div class="point point-bottom point-left">
+                </div>
+              </div>
+              <div class='face' data-id='5'>
+                <div class="point point-top point-right">
+                </div>
+                <div class="point point-top point-left">
+                </div>
+                <div class="point point-middle point-center">
+                </div>
+                <div class="point point-bottom point-right">
+                </div>
+                <div class="point point-bottom point-left">
+                </div>
+              </div>
+              <div class='face' data-id='3'>
+                <div class="point point-top point-right">
+                </div>
+                <div class="point point-middle point-center">
+                </div>
+                <div class="point point-bottom point-left">
+                </div>
+              </div>
+              <div class='face' data-id='4'>
+                <div class="point point-top point-right">
+                </div>
+                <div class="point point-top point-left">
+                </div>
+                <div class="point point-bottom point-right">
+                </div>
+                <div class="point point-bottom point-left">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button class="roll-btn">ROLL</button>
+        </div>
+      </div>
+  `,
   styleUrls: ['./game-board.component.css']
 })
 export class GameBoardComponent implements OnInit {
