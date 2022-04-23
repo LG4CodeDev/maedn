8bdf13cd211841fa9c6138cf348b3a41b8ad6f33
aschulte201 <a.schulte201@gmail.com>
2022-04-23 09:47:14 +0200

    generated components

---
+++ frontend/src/app/pages/game-board/game-board.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GameBoardRoutingModule } from './game-board-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GameBoardRoutingModule
+  ]
+})
+export class GameBoardModule { }
