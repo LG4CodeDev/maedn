ef56ce68acd97030cebf42d7718cb7837c96d61b
aschulte201 <a.schulte201@gmail.com>
2022-05-06 10:40:56 +0200

    generated component lobby

---
+++ frontend/src/app/pages/lobby/lobby/lobby.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lobby',
+  template: '',
+  styleUrls: ['./lobby.component.css']
+})
+export class LobbyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
