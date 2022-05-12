8d1873bb6ecbd02f53f2ca925c1c4e9422858c12
aschulte201 <a.schulte201@gmail.com>
2022-05-12 10:12:43 +0200

    added auth and new api paths

--- frontend/src/app/pages/lobby/lobby/lobby.component.ts
+++ frontend/src/app/pages/lobby/lobby/lobby.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-lobby',
   template: `
     <div id="lobby" nz-row [nzGutter]="{ xs: 8, sm: 16, md: 24, lg: 32 }">
-<!--      LEADERBOARD-->
+      <!--      LEADERBOARD-->
       <div id="leaderboard" class="gutter-row" nz-col nzFlex="7">
 
       </div>
-<!--      END OF LEADERBOARD-->
+      <!--      END OF LEADERBOARD-->
 
-<!--      GAME SELECTION-->
+      <!--      GAME SELECTION-->
       <div id="game_selection" class="gutter-row" nz-col nzFlex="10">
         <div nz-col>
           <div nz-row nzJustify="center">
@@ -46,20 +46,21 @@ import {HttpClient} from "@angular/common/http";
           </div>
         </div>
       </div>
-<!--      END OF GAME SELECTION-->
+      <!--      END OF GAME SELECTION-->
 
-<!--      STATISTICS-->
+      <!--      STATISTICS-->
       <div id="statistics" class="gutter-row" nz-col nzFlex="7">
 
       </div>
-<!--      END OF STATISTICS-->
+      <!--      END OF STATISTICS-->
     </div>
   `,
   styleUrls: ['./lobby.component.css']
 })
 export class LobbyComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.getStats();
@@ -70,13 +71,18 @@ export class LobbyComponent implements OnInit {
   }
 
   getStats(): void {
-    this.http.get<any>('http://167.235.24.74:4000/api/getUserStats/' + JSON.parse(localStorage.getItem('currentUser')).userid, {
-    observe: "response",
-  },
-  ).subscribe(response => {
-    console.log(response)
-  });
-}
+    console.log(JSON.parse(localStorage.getItem('currentUser')));
+    this.http.get<any>('https://spielehub.server-welt.com/api/getUserStats/' + JSON.parse(localStorage.getItem('currentUser')).userid, {
+
+        headers: {
+          'authorization': "Bearer " + JSON.parse(localStorage.getItem('currentUser')).token,
+        },
+        observe: "response",
+      },
+    ).subscribe(response => {
+      console.log(response)
+    });
+  }
 
 
 }
