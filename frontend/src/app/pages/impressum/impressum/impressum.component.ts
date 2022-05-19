bd7f097d302d804846f7d547b0a69da2845810d0
DESKTOP-ANTON\utzan <alien-code@outlook.de>
2022-05-19 15:37:34 +0200

    Filled impressum with real data

--- frontend/src/app/pages/impressum/impressum/impressum.component.ts
+++ frontend/src/app/pages/impressum/impressum/impressum.component.ts
@@ -19,14 +19,19 @@ export class ImpressumComponent implements OnInit {
     document.getElementById('names').innerHTML = '<h1 style="color: white;">Impressum</h1>\n' +
       '\n' +
       '<h2 style="color: white;">Angaben gem&auml;&szlig; &sect; 5 TMG</h2>\n' +
-      '<p>Max Musterman<br />\n' +
-      'Musterstra&szlig;e 3<br />\n' +
-      '12345 Musterstadt</p>\n' +
+      '<p>Benjamin Arp, Max Dürr, Tomasz Kania, Alexander Schulte und Anton Utz<br />\n' +
+      'Rotebühlplatz 41<br />\n' +
+      '70178 Stuttgart</p>\n' +
       '\n' +
       '<h2 style="color: white;">Kontakt</h2>\n' +
-      '<p class="text">Telefon: 012334567<br />\n' +
-      'Telefax: 0123345671<br />\n' +
-      'E-Mail: musterman@muster.de</p>\n' +
+      '<p class="text">Tel. +49 711 1849 4513<br />\n' +
+      'Fax +49 711 1849 4510<br />\n' +
+      'E-Mail: <br />\n' +
+      '<div style="margin-left: 20px">inf20142@lehre.dhbw-stuttgart.de,<br />\n' +
+      'inf20129@lehre.dhbw-stuttgart.de,<br />\n' +
+      'inf20086@lehre.dhbw-stuttgart.de, <br />\n' +
+      'inf20161@lehre.dhbw-stuttgart.de, <br />\n' +
+      'inf20005@lehre.dhbw-stuttgart.de </div></p>\n' +
       '\n' +
       '<p class="text">Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>';
   }
