1a73f8ab260ec7d92c17fa103bd047b67fc2dff8
aschulte201 <a.schulte201@gmail.com>
2022-05-19 22:13:01 +0200

    save

--- frontend/src/app/pages/lobby/lobby/lobby.component.ts
+++ frontend/src/app/pages/lobby/lobby/lobby.component.ts
@@ -135,8 +135,8 @@ joinRandom(): void {
         observe: "response",
       },
     ).subscribe(response => {
-      console.log(response);
       document.getElementById('profilePicture').setAttribute('src', response.body['image']);
+      document.getElementById('updateImagePreview').setAttribute('src', response.body['image']);
       document.getElementById('username').innerText =
         response.body['username'];
       this.updateAccount.patchValue({
@@ -188,36 +188,38 @@ joinRandom(): void {
     userWrapper.appendChild(thead);
 
     for(let i = 0; i<Object.keys(body).length; i++){
-      let userItemWrapper = this.renderer.createElement('tr');
-      userItemWrapper.classList.add("tr");
+      for(let i = 0; i<2; i++){
+        let userItemWrapper = this.renderer.createElement('tr');
+        userItemWrapper.classList.add("tr");
 
-      let place = this.renderer.createElement('td');
-      place.innerText = i+1;
-      place.classList.add("td");
+        let place = this.renderer.createElement('td');
+        place.innerText = i+1;
+        place.classList.add("td");
 
-      let tusernameWrapper = this.renderer.createElement('td');
-      tusernameWrapper.innerText = body[i.toString()]['username'];
-      tusernameWrapper.classList.add("td");
+        let tusernameWrapper = this.renderer.createElement('td');
+        tusernameWrapper.innerText = body[i.toString()]['username'];
+        tusernameWrapper.classList.add("td");
 
-      let levelWrapper = this.renderer.createElement('td');
-      levelWrapper.innerText = body[i.toString()]['level'];
-      levelWrapper.classList.add("td");
+        let levelWrapper = this.renderer.createElement('td');
+        levelWrapper.innerText = body[i.toString()]['level'];
+        levelWrapper.classList.add("td");
 
-      let twinsWrapper = this.renderer.createElement('td');
-      twinsWrapper.innerText = body[i.toString()]['wins'];
-      twinsWrapper.classList.add("td");
+        let twinsWrapper = this.renderer.createElement('td');
+        twinsWrapper.innerText = body[i.toString()]['wins'];
+        twinsWrapper.classList.add("td");
 
-      let WR = this.renderer.createElement('td');
-      WR.innerText = body[i.toString()]['winningRate'];
-      WR.classList.add("td");
+        let WR = this.renderer.createElement('td');
+        WR.innerText = body[i.toString()]['winningRate'];
+        WR.classList.add("td");
 
-      userItemWrapper.appendChild(place);
-      userItemWrapper.appendChild(tusernameWrapper);
-      userItemWrapper.appendChild(levelWrapper);
-      userItemWrapper.appendChild(twinsWrapper);
-      userItemWrapper.appendChild(WR);
+        userItemWrapper.appendChild(place);
+        userItemWrapper.appendChild(tusernameWrapper);
+        userItemWrapper.appendChild(levelWrapper);
+        userItemWrapper.appendChild(twinsWrapper);
+        userItemWrapper.appendChild(WR);
 
-      userWrapper.appendChild(userItemWrapper);
+        userWrapper.appendChild(userItemWrapper);
+      }
     }
     lb.appendChild(userWrapper);
   }
@@ -275,11 +277,12 @@ joinRandom(): void {
             },
           },
         ).subscribe(response => {
-          console.log(response)
-          if (response.status == 201) {
+          if (response.status == 200) {
             document.getElementById("update-form").style.display = "none";
             document.getElementById("userInformation").style.display = "block";
             this.snackBar.showSnackBar('green', 'Update successful!');
+            this.getUserInfo();
+            this.getLeaderboard();
           }
         });
       });
